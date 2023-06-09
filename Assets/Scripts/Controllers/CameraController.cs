916d4fbf37891a78e5bb6e2b025b683e54091e7f
ademsoydan <ademm.soydan@gmail.com>
2023-06-09 17:37:37 +0300

    Add camera movement

---
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform character;
+    [SerializeField] private Vector3 offset;
+
+    private void Awake()
+    {
+        offset = transform.position - character.position;
+    }
+    private void LateUpdate()
+    {
+        transform.position = character.position + offset;
+    }
+}
